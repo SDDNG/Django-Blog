1df8fd98d49863bd4f5a84782f6606cb0f5d0d1e
SDDNG <davidangraham@gmail.com>
2022-06-20 15:09:21 +0000

    commit deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-sddng.herokuapp.com', 'localhost ']
+ALLOWED_HOSTS = ['codestar-sddng.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -132,8 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join[BASE_DIR, 'staticfiles']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
